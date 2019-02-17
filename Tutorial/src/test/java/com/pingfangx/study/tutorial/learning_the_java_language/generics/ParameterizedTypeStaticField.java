46befd3c6b6bbd591c968b462a6f5aa01b6537b4
pingfangx <pingfangx@pingfangx.com>
2019-02-17 17:30:26 +0800

    add

---
+++ Tutorial/src/test/java/com/pingfangx/study/tutorial/learning_the_java_language/generics/ParameterizedTypeStaticField.java
@@ -0,0 +1,28 @@
+package com.pingfangx.study.tutorial.learning_the_java_language.generics;
+
+import org.junit.Test;
+
+/**
+ * @author pingfangx
+ * @date 2019/1/8
+ */
+public class ParameterizedTypeStaticField {
+
+    //泛型类不能继承 'java.lang.Throwable'
+    //static class Animal<T> extends Throwable{
+    static class Animal<T> {
+        public static int count;
+        //无法声明类型为类型参数的静态字段
+        //private static T t;
+
+        public Animal(T t) {
+            count++;
+        }
+    }
+
+    @Test
+    public void test() {
+        new Animal<>("");
+        System.out.println(Animal.count);
+    }
+}
---
+++ Tutorial/src/test/java/com/pingfangx/study/tutorial/learning_the_java_language/generics/UpperBoundsTest.java
@@ -0,0 +1,12 @@
+package com.pingfangx.study.tutorial.learning_the_java_language.generics;
+
+import java.util.List;
+
+/**
+ * @author pingfangx
+ * @date 2019/1/7
+ */
+public class UpperBoundsTest {
+    private void test(List<? extends Number> list) {
+    }
+}
