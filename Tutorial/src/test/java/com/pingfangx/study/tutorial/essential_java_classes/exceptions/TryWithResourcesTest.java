83a83dacb645f7615d35bd3b8740b6ad27e254ef
pingfangx <pingfangx@pingfangx.com>
2019-10-08 18:45:44 +0800

    添加测试

--- Tutorial/src/test/java/com/pingfangx/study/tutorial/essential_java_classes/exceptions/TryWithResourcesTest.java
+++ Tutorial/src/test/java/com/pingfangx/study/tutorial/essential_java_classes/exceptions/TryWithResourcesTest.java
@@ -1,9 +1,11 @@
 package com.pingfangx.study.tutorial.essential_java_classes.exceptions;
 
+import org.junit.Test;
+
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
 
 /**
  * @author pingfangx
@@ -33,6 +35,104 @@ public class TryWithResourcesTest {
             br.readLine();
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    private void suppressedException() throws Exception {
+        try (BufferedReader br = new BufferedReader(new FileReader(""))) {
+            System.out.println(br.readLine());
+            int i = 1 / 0;
+        }
+    }
+
+    /**
+     * 测试抑制的异常
+     */
+    @Test
+    public void test_suppressedException() {
+        try {
+            suppressedException();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    class TestResource implements AutoCloseable {
+        private boolean isOpen;
+
+        public void open() {
+            isOpen = true;
+        }
+
+        @Override
+        public void close() throws IllegalStateException {
+            System.out.println("close,isOpen=" + isOpen);
+            if (isOpen) {
+                isOpen = false;
+            } else {
+                throw new IllegalStateException("文件已关闭");
+            }
+        }
+    }
+
+    /**
+     * close in try
+     * close,isOpen=true
+     * try finish
+     * close,isOpen=false
+     * 拦截到异常:文件已关闭
+     * finally
+     */
+    private void suppressedException2() {
+        try (TestResource resource = new TestResource()) {
+            resource.open();
+            System.out.println("close in try");
+            resource.close();
+            System.out.println("try finish");
+        } catch (IllegalStateException e) {
+            System.out.println("拦截到异常:" + e.getLocalizedMessage());
+        } finally {
+            //错误，引用不到 resource
+            //resource.close();
+            System.out.println("finally");
+        }
+    }
+
+    /**
+     * close in try
+     * close,isOpen=true
+     * try finish
+     * close,isOpen=false
+     * finally
+     * 外部拦截到异常:/ by zero
+     * 抑制的异常:[java.lang.IllegalStateException: 文件已关闭]
+     */
+    private void suppressedException3() {
+        try (TestResource resource = new TestResource()) {
+            resource.open();
+            System.out.println("close in try");
+            resource.close();
+            System.out.println("try finish");
+            int i = 1 / 0;
+        } catch (IllegalStateException e) {
+            System.out.println("拦截到异常:" + e.getLocalizedMessage());
+        } finally {
+            //错误，引用不到 resource
+            //resource.close();
+            System.out.println("finally");
+        }
+    }
+
+    /**
+     * 测试抑制的异常
+     */
+    @Test
+    public void test_suppressedException2() {
+        try {
+            suppressedException3();
+        } catch (Exception e) {
+            System.out.println("外部拦截到异常:" + e.getLocalizedMessage());
+            System.out.println("抑制的异常:" + Arrays.toString(e.getSuppressed()));
         }
     }
 }
